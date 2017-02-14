220b771763ff94303950ef15c5ad31109fad93ce
R-adachi <s153002@matsu.shimane-u.ac.jp>
2017-02-14 14:45:54 +0900

    make book

--- my_web_apps/db/schema.rb
+++ my_web_apps/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214025227) do
+ActiveRecord::Schema.define(version: 20170214053656) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
