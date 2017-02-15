dae064c7588de808629b4403beb7ab67adb22cda
R-adachi <s153002@matsu.shimane-u.ac.jp>
2017-02-15 14:15:51 +0900

    add taskapp create

--- my_web_apps/app/controllers/projects_controller.rb
+++ my_web_apps/app/controllers/projects_controller.rb
@@ -4,10 +4,22 @@ class ProjectsController < ApplicationController
     end
 
 def show
-	@project=Project.find(params[:id])
+	@project = Project.find(params[:id])
 end
 def  new
 	@project = Project.new
 end
+def create
+	@project = Project.new(project_params)
+	@project.save
+	redirect_to projects_path
 end
+private
+
+def project_params
+	params[:project].permit(:title)
+end
+
+end
+
 
