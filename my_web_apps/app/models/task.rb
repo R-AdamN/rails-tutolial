9bf9f3b7cc720de85b9aa4e69873c03fe1934191
R-adachi <s153002@matsu.shimane-u.ac.jp>
2017-02-16 11:24:45 +0900

    taskapp finish

--- my_web_apps/app/models/task.rb
+++ my_web_apps/app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
 	validates :title, presence: true
-
+	scope :unfinished, ->{ where(done: false)}
 
 end
--- my_web_apps/config/routes.rb
+++ my_web_apps/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :projects do
  resources :tasks, only: [:create, :destroy]
 end
+  post' /projects/:projects_id/tasks/:id/toggle' => 'tasks#toggle'
   root 'projects#index'
   resources :users
   resources :books
