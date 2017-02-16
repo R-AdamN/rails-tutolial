b8b01d562792c38562ed42c0e5d824d9bcd02b6c
R-adachi <s153002@matsu.shimane-u.ac.jp>
2017-02-16 09:21:17 +0900

    add taskapp taskdb&con

--- my_web_apps/db/schema.rb
+++ my_web_apps/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215014702) do
+ActiveRecord::Schema.define(version: 20170215075831) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20170215014702) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "done",       default: false
+    t.integer  "project_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
